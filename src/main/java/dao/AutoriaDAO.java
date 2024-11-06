0448c3730437562b8247f6906b3d5e0e0cb3e26a
DavidPuerto <tutos6782@gmail.com>
2024-11-06 19:26:17 +0100

    acabando autoriaDAO

--- src/main/java/dao/AutoriaDAO.java
+++ src/main/java/dao/AutoriaDAO.java
@@ -1,10 +1,96 @@
 package dao;
 
 import biblioteca.Autoria;
+import biblioteca.Libro;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AutoriaDAO {
+    private static int instancias = -1;
 
-    public static void insertarAutoria(Autoria autor){
-        String sql = ""
+    public static int insertarAutoria(Autoria autor){  //Funciona
+        String sql = "insert into autorias" +
+                    "(id,nombre,apellido)" +
+                    "values(?,?,?)";
+        try(Connection con = Conexion.conectar()){
+            PreparedStatement s = con.prepareStatement(sql);
+            s.setInt(1, autor.getId());
+            s.setString(2, autor.getNombre());
+            s.setString(3, autor.getApellido());
+            instancias = s.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return instancias;
     }
+
+    public static int leerAutoria(int id){    //testear
+        Autoria a1 = null;
+        String sql = "select * from autorias where id = ?";
+        instancias = 0;
+
+        try(Connection con = Conexion.conectar()){
+            PreparedStatement p = con.prepareStatement(sql);
+                p.setInt(1, id);
+                    ResultSet rs = p.executeQuery();
+                    while(rs.next()){       //COMPROBAR ESTO Q NO ME FIO
+                        instancias++;
+                    }
+                    if(rs.next()){
+                        int num = rs.getInt(1);
+                            String nombre = rs.getString("nombre");
+                                String apellido = rs.getString("apellidos");
+                                    a1 = new Autoria(num,nombre,apellido);
+                    }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return instancias;
+    }
+
+    public static int actualizarAutoria(Autoria autor){    //TEST
+        String sql = "update autorias set nombre = ?, apellido = ? where id = ?";
+
+        try(Connection con = Conexion.conectar()){
+            PreparedStatement p = con.prepareStatement(sql);
+                p.setString(1, autor.getNombre());
+                p.setString(2, autor.getApellido());
+                p.setInt(3, autor.getId());
+                instancias = p.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return instancias;
+    }
+
+    public static int eliminarAutoria(int id){ //TEST
+        String sql = "delete from autorias where id = ?";
+
+        try(Connection con = Conexion.conectar()){
+            PreparedStatement p = con.prepareStatement(sql);
+            instancias = p.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return instancias;
+    }
+
+    public static int leerTodasAutorias(HashMap <Integer, Autoria> autores){//test
+        int id;
+        for(Map.Entry<Integer, Autoria> hm : autores.entrySet()){
+            id = hm.getKey();
+            instancias = leerAutoria(id);
+        }
+        return instancias;
+    }
+
+    public static int crearActualizarAutorias(){
+
+    }
+
 }
