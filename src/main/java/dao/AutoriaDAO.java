e83757d8435f0ff6cee65884f46dabce172181f3
DavidPuerto <tutos6782@gmail.com>
2024-11-06 18:47:14 +0100

    Conectar y tablas funciona

--- src/main/java/dao/AutoriaDAO.java
+++ src/main/java/dao/AutoriaDAO.java
@@ -1,4 +1,10 @@
 package dao;
 
+import biblioteca.Autoria;
+
 public class AutoriaDAO {
+
+    public static void insertarAutoria(Autoria autor){
+        String sql = ""
+    }
 }
--- src/main/java/dao/Conexion.java
+++ src/main/java/dao/Conexion.java
@@ -7,23 +7,24 @@ import java.sql.Statement;
 
 public class Conexion {
 
-    protected static Connection conectar() throws SQLException {
-        String user = "";
-        String pass = "";
+    protected static Connection conectar() throws SQLException {    //furula
+        String user = "root";
+        String pass = "root";
         String url = "jdbc:mysql://localhost:3306/biblioteca?useSSL=false";
         Connection con = DriverManager.getConnection(url, user ,pass);
         return con;
     }
 
-    public void createTables(){ //comprobar todo
-        String sqlAu ="create table if not exists autores"+
+    public void createTables(){ //furula
+        String sqlAu ="create table if not exists autorias"+
                 "(id varchar(50) primary key," +
                 "nombre varchar(50) not null," +
-                "apellido varchar(50))";
-        String sqlLib = "create table if not exist libros"+
+                "apellido varchar(50) not null)";
+        String sqlLib = "create table if not exists libros"+
                  "(isbn varchar(50) primary key," +
                  "titulo varchar(50) not null," +
-                 "id autoria varchar(50))";
+                 "idautoria varchar(50) not null," +
+                 "foreign key (idautoria) references autorias(id))";
 
         try(Connection c = conectar()){
             Statement st = c.createStatement();
